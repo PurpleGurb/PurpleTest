be415c7f77af00fc5f911ecc578343c8e36b8e36
PurpleGurb <GurbSingh@gmail.com>
2018-03-10 20:18:48 +0000

    Content Security Policy added to protect against click jacking

--- OrangeBricks.Web/App_Start/FilterConfig.cs
+++ OrangeBricks.Web/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+
+using OrangeBricks.Web.Attributes;
 
 namespace OrangeBricks.Web
 {
@@ -8,6 +9,7 @@ namespace OrangeBricks.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new ContentSecurityPolicyFilterAttribute());
         }
     }
 }
---
+++ OrangeBricks.Web/Attributes/ContentSecurityPolicyFilterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace OrangeBricks.Web.Attributes
+{
+    public class ContentSecurityPolicyFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var response = filterContext.HttpContext.Response;
+            response.AddHeader("Content-Security-Policy", "script-src 'self'");
+            response.AddHeader("X-WebKit-CSP", "script-src 'self'");
+            response.AddHeader("X-Content-Security-Policy", "script-src 'self'");
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
